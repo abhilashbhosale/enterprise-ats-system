9e7af0f75b7b4f621f33e8b13a1ac7d865f6df8a
Abhilash Bhosale <abhilashbhosale12@gmail.com>
2026-03-27 16:47:15 +0530

    Add database package initialization

---
+++ database/__init__.py
@@ -0,0 +1,8 @@
+"""
+Database Package
+Author: Abhilash Bhosale
+GitHub: https://github.com/abhilashbhosale
+"""
+from database.connection import DatabaseConnection
+
+__all__ = ['DatabaseConnection']
